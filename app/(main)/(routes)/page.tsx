d8d73c296e6f35fd544483a03f77b348abf98b2c
Naman Bhatia <bhatia.naman@hotmail.com>
2024-01-10 16:25:20 +0530

    added: toggler for dark and light mode

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,9 +1,11 @@
+import { ModeToggle } from "@/components/modeToggle";
 import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div className="h-screen">
       <UserButton afterSignOutUrl="/" />
+      <ModeToggle />
     </div>
   );
 }
