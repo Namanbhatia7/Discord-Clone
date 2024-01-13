265f9106aca64f576876efdde110a362b9f45437
Naman Bhatia <bhatia.naman@hotmail.com>
2024-01-14 00:37:18 +0530

    added: integration with uploadthing, setup code with dropzone

--- components/modals/initial-modal.tsx
+++ components/modals/initial-modal.tsx
@@ -23,6 +23,7 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { useEffect, useState } from "react";
+import FileUpload from "../file-upload";
 
 const formSchema = z.object({
   name: z.string().min(1, {
@@ -73,7 +74,24 @@ const InitialModal = () => {
           <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
             <div className="space-y-8 px-6">
               <div className="flex items-center justify-center text-center">
-                TODO: Upload Image
+                <FormField
+                  control={form.control}
+                  name="imageUrl"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel className="uppercase text-xs font-bold text-zinc-500 dark:text-secondary/70">
+                        Server name
+                      </FormLabel>
+                      <FormControl>
+                        <FileUpload
+                          endpoint="serverImage"
+                          value={field.value}
+                          onChange={field.onChange}
+                        />
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
               </div>
               <FormField
                 control={form.control}
