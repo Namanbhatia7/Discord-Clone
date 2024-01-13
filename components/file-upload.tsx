49b9347314c740f6f81bbc23d659d5549d05f153
Naman Bhatia <bhatia.naman@hotmail.com>
2024-01-14 00:48:13 +0530

    added: functionality to display pic after successfully uploading it

--- components/file-upload.tsx
+++ components/file-upload.tsx
@@ -1,7 +1,10 @@
 "use client";
 
+import { X } from "lucide-react";
+import Image from "next/image";
 import { UploadDropzone } from "@/lib/uploadthing";
 import "@uploadthing/react/styles.css";
+import next from "next";
 
 interface FileUploadProps {
   onChange: (url?: string) => void;
@@ -10,6 +13,15 @@ interface FileUploadProps {
 }
 
 const FileUpload = ({ onChange, value, endpoint }: FileUploadProps) => {
+  const fileType = value?.split(".").pop();
+
+  if (value && fileType !== "pdf") {
+    return (
+      <div className="relative h-20 w-20">
+        <Image fill src={value} alt="Upload" className="rounded-full" />
+      </div>
+    );
+  }
   return (
     <UploadDropzone
       endpoint={endpoint}
